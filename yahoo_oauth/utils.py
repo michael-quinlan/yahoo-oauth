e5fd4f606b7dfd431beed585f6114557fa540bf8
michael-quinlan <michael.quinlan@gmail.com>
2022-08-24 23:55:53 -0700

    Use yaml.FullLoader

--- yahoo_oauth/utils.py
+++ yahoo_oauth/utils.py
@@ -61,7 +61,7 @@ def yaml_get_data(filename):
     """Get data from .yml file
     """
     with open(filename, 'rb') as fd:
-        yaml_data = yaml.load(fd)
+        yaml_data = yaml.load(fd, Loader=yaml.FullLoader)
         return yaml_data
     return False
 
